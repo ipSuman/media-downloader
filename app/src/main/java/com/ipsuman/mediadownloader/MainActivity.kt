fb741644b13614c6372a874607cbe0b6a597b86d
Suman Bhandari <79626165+ipSuman@users.noreply.github.com>
2026-08-09 23:50:10 +0530

    Add Android WebView activity
    Create app/src/main/java/com/ipsuman/mediadownloader/MainActivity.kt

---
+++ app/src/main/java/com/ipsuman/mediadownloader/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ipsuman.mediadownloader
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+
+        webView.webViewClient = WebViewClient()
+
+        setContentView(webView)
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
